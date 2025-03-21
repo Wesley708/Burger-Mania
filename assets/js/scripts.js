56383661f1858272007f6b7fa2a231d67ea92476
Wesley Alves do Nascimento <65147671+Wesley708@users.noreply.github.com>
2025-03-21 18:23:09 +0000

    Mudança de IDE

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+let hamburguer = {
+    nome: "Hamburger Classico Americano",
+    Ingredientes: "Ingredientes: Carne moída, sal, pimenta, queijo cheddar, alface, tomate, cebola roxa, picles, ketchup, mostarda e maionese. Dica: Use carne com um pouco de gordura para um hambúrguer mais suculento.",
+    imagem: "classico-americano"
+}
+
+console.log(hamburguer)
